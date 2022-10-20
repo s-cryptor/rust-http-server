045b9495a6200cddfad71c9d43ac78d8462f3333
s-cryptor <grisga2001@gmail.com>
2022-10-20 17:13:28 +0300

    paths

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -14,8 +14,10 @@ mod front_of_house {
     }
 }
 
+pub use crate::front_of_house::hosting;
+
 pub fn eat_at_restaurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
     front_of_house::hosting::add_to_waitlist();
 
     // Order a breakfast in the summer with Rye toast
