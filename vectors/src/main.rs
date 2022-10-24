06bab1e577f892dcd512501e472286e90c25334b
s-cryptor <grisga2001@gmail.com>
2022-10-24 15:52:23 +0300

    vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let v: Vec<i32> = Vec::new();
+    let mut v = vec![1, 2, 3];
+    v.push(7);
+
+    let v = vec![1, 2, 3];
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element"),
+    }
+
+    // let mut v = vec![1, 2, 3, 4, 5];
+    // let first = &v[0];
+    // v.push(6);
+    // println!("The first element is: {}", first);
+
+    let v = vec![100, 32, 57];
+    for i in &v {
+        println!("{}", i);
+    }
+
+    let mut v = vec![100, 32, 57];
+    for i in &mut v {
+        *i += 50;
+    }
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
