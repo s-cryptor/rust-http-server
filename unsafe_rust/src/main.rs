9bc86cf415abea8061e0f55398d4e0837c97dc8a
s-cryptor <grisga2001@gmail.com>
2022-12-21 09:29:54 +0300

    safe abstraction

--- unsafe_rust/src/main.rs
+++ unsafe_rust/src/main.rs
@@ -1,3 +1,5 @@
+use std::slice;
+
 fn main() {
     let mut num = 5;
     let r1 = &num as *const i32;
@@ -15,5 +17,25 @@ fn main() {
 
     unsafe {
         dangerous();
+    }
+
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+    let r = &mut v[..];
+    let (a, b) = r.split_at_mut(3);
+    assert_eq!(a, &mut [1, 2, 3]);
+    assert_eq!(b, &mut [4, 5, 6]);
+}
+
+fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
     }
 }
